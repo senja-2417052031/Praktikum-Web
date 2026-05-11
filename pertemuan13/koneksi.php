30205b90720ec08da3cb925418b895e23cf5eea5
Senja <senjaarina14@gmail.com>
2026-05-11 08:48:25 +0700

    pertemuan 13

---
+++ pertemuan13/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "paktium_login";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
